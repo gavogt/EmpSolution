4545cae79f62fa4c924dbde27f152063caad719c
gavogt <gvogt@asu.edu>
2018-05-18 15:14:39 -0700

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string employerNameInputing = "";
+
+            Console.WriteLine("*** Employee Solution ***");
+
+            Console.WriteLine("What is your name?");
+            employerNameInputing = Console.ReadLine();
+
+            if (employerNameInputing == string.Empty)
+            {
+                WelcomeName();
+            }
+            else
+            {
+                WelcomeName(employerNameInputing);
+            }
+        }
+
+        // Welcome the inputter
+        static void WelcomeName()
+        {
+            Console.WriteLine("You must have a name");
+        }
+
+        // Welcome overloaded
+        static void WelcomeName(string name)
+        {
+            Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
